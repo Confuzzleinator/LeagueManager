f4078d7c7dfc50c7e88301dfefad07aef8573e8a
Confuzzleinator <javen.kazebee@oit.edu>
2020-08-25 13:04:24 -0700

    Initialized repo

---
+++ ui/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <v-btn color="primary" @click="OnClick">{{btnText}}</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data: () => {
+    return {
+          btnText: 'Sim'
+    }
+  },
+  methods: {
+    OnClick() {
+      this.btnText = Click()
+    }
+  }
+}
+</script>
